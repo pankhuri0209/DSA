24bb32df70a32919d9b93f0962f654f4791bb69f
Pankhuri Gupta <pankhurig1@gmail.com>
2023-10-08 20:10:21 +0530

    sort

---
+++ src/leetcode/finderror.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class finderror {
+
+    public static int[] findErrorNums(int[] nums) {
+        int[] arr=new int[nums.length];
+        int[] result=new int[2];
+
+        for(int i:nums){
+            arr[i-1]++;
+        }
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==2){
+                result[0]=i+1;
+            }
+            if(arr[i]==0){
+                result[1]=i+1;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args)
+    {
+        // int [] arr= new int[]{44,22,55,11,66};
+        int[] arr= new int[]{3,2,3,4};
+        int res[]=findErrorNums(arr);
+
+        System.out.println(Arrays.toString(res));
+    }
+}
