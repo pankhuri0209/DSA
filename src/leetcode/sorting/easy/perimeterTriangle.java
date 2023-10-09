df13411c0473137933856b5ad531b422c1b8560a
Pankhuri Gupta <pankhurig1@gmail.com>
2023-10-09 17:17:11 +0530

    sort

---
+++ src/leetcode/sorting/easy/perimeterTriangle.java
@@ -0,0 +1,36 @@
+package leetcode.sorting.easy;
+
+import java.util.Arrays;
+
+public class perimeterTriangle {
+    public static int largestPerimeter(int[] nums) {
+        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        for (int k = n - 1; k >= 2; k--) {
+            int c = nums[k];
+            int b = nums[k - 1];
+            int a = nums[k - 2];
+
+            if (isValid(a, b, c)) {
+                return a + b + c;
+            }
+        }
+
+        return 0;
+    }
+
+    private static boolean isValid(int a, int b, int c) {
+        return a + b > c;
+    }
+    public static void main(String[] args)
+    {
+        int [] A= new int[]{2,1,2};
+        int[] B= new int[]{2,3};
+
+        int res=largestPerimeter(A);
+
+        System.out.println(res);
+    }
+}
