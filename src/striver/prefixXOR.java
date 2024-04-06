4aca16591405fcecd28a74f879b7dbacb836467e
Pankhuri Gupta <34907504+pankhuri0209@users.noreply.github.com>
2024-04-06 14:58:54 -0400

    prefix xor

---
+++ src/striver/prefixXOR.java
@@ -0,0 +1,38 @@
+package striver;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class prefixXOR {
+    public static int subarraysWithSumK(int []a, int b) {
+        // Write your code here
+        int n = a.length; //size of the given array.
+        int xr = 0;
+        Map<Integer, Integer> mpp = new HashMap<>(); //declaring the map.
+        mpp.put(xr, 1); //setting the value of 0.
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            // prefix XOR till index i:
+            xr = xr ^ a[i];
+
+            //By formula: x = xr^k:
+            int x = xr ^ b;
+
+            // add the occurrence of xr^k
+            // to the count:
+            if (mpp.containsKey(x)) {
+                cnt += mpp.get(x);
+            }
+
+            // Insert the prefix xor till index i
+            // into the map:
+            if (mpp.containsKey(xr)) {
+                mpp.put(xr, mpp.get(xr) + 1);
+            } else {
+                mpp.put(xr, 1);
+            }
+        }
+        return cnt;
+    }
+}
