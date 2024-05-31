b1cc711eb01f98e9d8c1b69b75d692f0477c28ed
Pankhuri Gupta <34907504+pankhuri0209@users.noreply.github.com>
2024-05-30 20:50:48 -0400

    xor

---
+++ src/striver/rotatell.java
@@ -0,0 +1,34 @@
+package striver;
+
+
+public class rotatell {
+    public ListNode rotateRight(ListNode head, int k) {
+
+        if(head==null || head.next==null || k==0)
+        {
+            return head;
+        }
+
+        ListNode curr= head;
+        int total=1;
+
+        while(curr.next!=null)
+        {
+            ++total;
+            curr= curr.next;
+        }
+        curr.next= head;
+        k= k % total;
+        k= total-k;
+        while(k-- >0)
+        {
+            curr= curr.next;
+
+        }
+        head= curr.next;
+        curr.next= null;
+        return head;
+
+
+    }
+}
