0b1351eac434bfc6bddf6112dde786b4ebf8e120
Pankhuri Gupta <34907504+pankhuri0209@users.noreply.github.com>
2024-06-28 20:37:19 -0400

    xor

--- src/Recursion/subsetsum/Solution.java
+++ src/Recursion/subsetsum/Solution.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.util.*;
 //User function Template for Java//User function Template for Java
 class Solution{
-    ArrayList<Integer> subsetSums(ArrayList<Integer> arr, int N){
+   static ArrayList<Integer> subsetSums(ArrayList<Integer> arr, int N){
         // code here
         ArrayList<Integer> ds= new ArrayList<>();
         ssh(0,0,arr,N,ds);
@@ -11,7 +11,7 @@ class Solution{
         return ds;
 
     }
-    void ssh(int ind, int sum, ArrayList<Integer> arr, int N, ArrayList<Integer> ds)
+    static void ssh(int ind, int sum, ArrayList<Integer> arr, int N, ArrayList<Integer> ds)
     {
         if(ind == N)
         {
@@ -21,6 +21,19 @@ class Solution{
         ssh(ind+1,sum+arr.get(ind),arr,N,ds); //pick the element
         ssh(ind+1,sum,arr,N,ds);        //not pick the element
 
+    }
+    public static void main(String args[]) {
+
+
+        ArrayList < Integer > arr = new ArrayList < > ();
+        arr.add(2);
+        arr.add(3);
+      //  arr.add(2);
+        ArrayList < Integer > ans = subsetSums(arr, arr.size());
+        Collections.sort(ans);
+        System.out.println("The sum of each subset is ");
+        for (int i = 0; i < ans.size(); i++)
+            System.out.print(ans.get(i) + " ");
     }
 
 }
