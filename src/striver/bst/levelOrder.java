85b4c7cf3f8e11eac6112ae9c74bc912d7a27078
Pankhuri Gupta <34907504+pankhuri0209@users.noreply.github.com>
2024-07-17 19:13:53 -0400

    level order in bst

---
+++ src/striver/bst/levelOrder.java
@@ -0,0 +1,48 @@
+package striver.bst;
+
+import sun.reflect.generics.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class levelOrder {
+
+    public List<List<Integer>> levelOrder(TreeNode root)
+    {
+        List<List<Integer>> result= new ArrayList<>();
+        if(root==null)
+        {
+            return result;
+
+        }
+        Queue<TreeNode> q= new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty())
+        {
+            int size = q.size();
+            List<Integer> level= new ArrayList<>();
+            for(int i=0;i<size;i++);
+            {
+                TreeNode curr= q.poll();
+                //List<Integer> level= new ArrayList<>();
+                level.add(curr.val);
+                if(curr.left !=null)
+                {
+                    q.add(curr.left);
+                }
+                if(curr.right !=null)
+                {
+                    q.add(curr.right);
+                }
+            }
+            result.add(level);
+        }
+        return result;
+
+    }
+
+
+}
