5bdf335089444a5d66d6dec8bdf322e8e7e948b0
Pankhuri Gupta <34907504+pankhuri0209@users.noreply.github.com>
2024-07-17 19:19:19 -0400

    max height in bst

---
+++ src/striver/bst/height.java
@@ -0,0 +1,12 @@
+package striver.bst;
+
+public class height {
+    public int maxDepth(TreeNode root)
+    {
+        if(root ==null)
+        {
+            return 0;
+        }
+        return 1+Math.max(maxDepth(root.left) , maxDepth(root.right));
+    }
+}
