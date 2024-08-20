6a98e8d8b4e2f9a242a87b39f7cd2999f57eb0ec
Pankhuri Gupta <34907504+pankhuri0209@users.noreply.github.com>
2024-08-19 21:33:37 -0400

    browserHistory

---
+++ src/roblox/browserHistory.java
@@ -0,0 +1,35 @@
+package roblox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class browserHistory {
+
+    private List<String> history;
+    private int currentIndeix;
+
+    public browserHistory(String homepage)
+    {
+        history = new ArrayList<>();
+        history.add(homepage);
+        currentIndeix=0;
+    }
+    public void visit(String url)
+    {
+        history = history.subList(0,currentIndeix+1);
+        history.add(url);
+        currentIndeix++;
+    }
+
+    public String back(int steps)
+    {
+        currentIndeix = Math.max(0, currentIndeix-steps);
+        return history.get(currentIndeix);
+    }
+    public String forward(int steps)
+    {
+        currentIndeix=Math.min(history.size()-1, currentIndeix+steps);
+        return  history.get(currentIndeix);
+    }
+
+}
