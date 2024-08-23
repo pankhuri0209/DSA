46e3f7038d8f99be4eddf3204c279d376cefa576
Pankhuri Gupta <34907504+pankhuri0209@users.noreply.github.com>
2024-08-23 15:02:13 -0400

    shuffle

---
+++ src/roblox/shuffle.java
@@ -0,0 +1,34 @@
+package roblox;
+
+public class shuffle {
+    public static int[] shuffle(int[] nums, int n) {
+        int pointer1=0, pointer2=0;
+        int[] res= new int[n];
+        if(n>0)
+        {
+            pointer1=0;
+            pointer2 = n;
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i%2!=0)
+            {
+                res[i]= nums[pointer2];
+                pointer2++;
+            }
+            else
+            {
+                res[i]= nums[pointer1];
+                pointer1++;
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args)
+    {
+        int[] num = new int[]{2,5,1,3,4,7};
+
+        int[] res= shuffle(num, 3);
+        System.out.println(res.toString());
+    }
+}
