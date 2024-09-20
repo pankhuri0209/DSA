71c46e37991498b281b94b45badebadd178d825a
Pankhuri Gupta <34907504+pankhuri0209@users.noreply.github.com>
2024-09-19 23:50:07 -0400

    nearestExit

---
+++ src/striver/graphs/shortestPathLength.java
@@ -0,0 +1,21 @@
+package striver.graphs;
+
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
+
+public class shortestPathLength {
+
+    public void shortestPathLength(int[][] graph)
+    {
+        if (graph.length ==1)
+        {
+           // return 0;
+
+        }
+        int n= graph.length;
+        Set<Integer> [] visited = new HashSet[n];
+        System.out.println();
+       // Queue<Node>
+    }
+}
