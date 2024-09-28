5fed8dcdc50236c7720d1a601c087828c248d830
pankhuri-neu <gupta.pankh@northeastern.edu>
2024-09-27 20:54:26 -0400

    hasPathSum

---
+++ src/striver/graphs/hasPathSum.java
@@ -0,0 +1,19 @@
+package striver.graphs;
+
+public class hasPathSum {
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        if(root==null)
+        {
+            return false;
+        }
+        if(root.left==null && root.right==null)
+        {
+            return (targetSum==root.val);
+        }
+        return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
+
+
+    }
+}
