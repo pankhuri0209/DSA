f5e9634abf033eeb84647206b64ebab93fcdf59b
pankhuri0209 <pankhurig1@gmail.com>
2024-10-01 00:05:48 -0400

    gcd

---
+++ src/blind75/gcdOfStrings.java
@@ -0,0 +1,23 @@
+package blind75;
+
+public class gcdOfStrings {
+
+    public String gcd(String str1, String str2) {
+        if(!(str1+str2).equals(str2+ str1))
+        {
+            return "";
+        }
+        int gcdLen= gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdLen);
+    }
+    private int gcd(int a, int b)
+    {
+        while(b!=0)
+        {
+            int temp= b;
+            b=a%b;
+            a= temp;
+        }
+        return a;
+    }
+}
