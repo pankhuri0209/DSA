bf6019f1fbdcebf2f3101974502f835f7b91736b
pankhuri0209 <pankhurig1@gmail.com>
2024-10-01 18:24:55 -0400

    remove duplicates

---
+++ src/blind75/removeADuplicates.java
@@ -0,0 +1,22 @@
+package blind75;
+
+public class removeADuplicates {
+
+    public  int removeDuplicates(int[] nums)
+    {
+        if(nums.length<=2)
+        {
+            return nums.length;
+        }
+        int writeIndex=2;
+
+        for (int i=2;i<nums.length;i++)
+        {
+            if (nums[i]!=nums[writeIndex-2])
+            {
+                nums[writeIndex++]=nums[i];
+            }
+        }
+        return writeIndex;
+    }
+}
